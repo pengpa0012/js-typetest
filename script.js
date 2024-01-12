a17ae66b3367b2646cdc3545b9d01125633dfe69
Pengpa0012 <peng.ilikha@gmail.com>
2024-01-12 23:27:44 +0800

    added char index checker

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const content = document.querySelector("#content")
+let index = 0
+
 
 window.addEventListener("keydown", (e) => {
-  content.textContent += e.key
+  const currCharacter = content.textContent.split("")[index]
+  if(e.key.toLocaleLowerCase() == currCharacter.toLocaleLowerCase()) {
+    console.log("CORRECT")
+    index++
+  } else {
+    console.log("WRONG")
+  }
 })
